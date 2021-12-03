c1f88a815632c14c3d9dcea22782aead2172dc1f
Thomas Zub <thomas.zub@codecentric.de>
2021-12-03 16:56:38 +0100

    Added day 1 execution

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,21 @@
-use crate::base::DayExecutable;
+use itertools::Itertools;
+use crate::base::{DayExecutable, read_lines};
 
 pub struct Day1Executable {}
 
 impl DayExecutable for Day1Executable {
     fn execute(&self) {
-        println!("Day1")
+        let lines_result = read_lines();
+        match lines_result {
+            Err(e) => println!("Error: {}", e),
+            Ok(lines) => self.process(lines)
+        }
+    }
+}
+
+impl Day1Executable {
+    fn process(&self, lines: Vec<String>) {
+        let result = lines.iter().map(|l| l.parse::<u32>().unwrap()).into_iter().tuple_windows::<(_,_)>().map(|(i1, i2)| if i1 < i2 { 1 } else { 0 } ).fold(0, |sum, v| sum + v);
+        println!("Result: {}", result);
     }
 }
