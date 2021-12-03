30388ec2fd76b2d3c92dd6f76e2de3996a2cd415
Thomas Zub <thomas.zub@codecentric.de>
2021-12-03 16:13:24 +0100

    Added file reading helper function

--- src/base.rs
+++ src/base.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::io::{Result, BufRead, BufReader};
+
 pub trait DayExecutable {
     fn execute(&self);
+}
+
+pub fn read_lines() -> Result<Vec<String>> {
+    File::open("./data/day1.txt").map(|f| BufReader::new(f).lines().into_iter().map(|l| l.unwrap()).collect())
 }
