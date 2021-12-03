8d64e3eb8433f81aabf2469e227998cd17164dd7
Thomas Zub <thomas.zub@codecentric.de>
2021-12-03 17:17:56 +0100

    Added solution for part 2, removed unnecessary into_iter

--- src/day1.rs
+++ src/day1.rs
@@ -8,17 +8,28 @@ impl DayExecutable for Day1Executable {
         let lines_result = read_lines("./data/day1.txt");
         match lines_result {
             Err(e) => println!("Error: {}", e),
-            Ok(lines) => self.process(lines),
+            Ok(lines) => self.process_part2(lines),
         }
     }
 }
 
 impl Day1Executable {
-    fn process(&self, lines: Vec<String>) {
+    #[allow(dead_code)]
+    fn process_part1(&self, lines: Vec<String>) {
         let result = lines
             .iter()
             .map(|l| l.parse::<u32>().unwrap())
-            .into_iter()
+            .tuple_windows::<(_, _)>()
+            .map(|(i1, i2)| if i1 < i2 { 1 } else { 0 })
+            .fold(0, |sum, v| sum + v);
+        println!("Result: {}", result);
+    }
+    fn process_part2(&self, lines: Vec<String>) {
+        let result = lines
+            .iter()
+            .map(|l| l.parse::<u32>().unwrap())
+            .tuple_windows::<(_, _, _)>()
+            .map(|(i1, i2, i3)| i1 + i2 + i3)
             .tuple_windows::<(_, _)>()
             .map(|(i1, i2)| if i1 < i2 { 1 } else { 0 })
             .fold(0, |sum, v| sum + v);
