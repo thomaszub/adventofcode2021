70f938f5515aa40175b539b132d31040060394d0
Thomas Zub <thomas.zub@codecentric.de>
2021-12-03 16:55:16 +0100

    Execution time output added

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ mod base;
 mod day1;
 
 use std::env;
+use std::time::Instant;
 use crate::base::DayExecutable;
 use crate::day1::Day1Executable;
 
 fn main() {
+    let start = Instant::now();
     let args: Vec<String> = env::args().collect();
     let executor: Option<Box<dyn DayExecutable>> = match args.get(1) {
         None => None,
@@ -20,4 +22,6 @@ fn main() {
         None => {println!("No known day name provided")}
         Some(exec) => {exec.execute()}
     }
+    let duration = start.elapsed();
+    println!("Execution time: {:?}", duration);
 }
