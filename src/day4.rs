2fcd55fe2ae3dc5dc334c59f93f4bd23e1675c02
Thomas Zub <thomas.zub@codecentric.de>
2021-12-10 08:35:58 +0100

    Added parsing of boards

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,5 @@
 use crate::base::read_lines;
+use crate::day4::Entry::Value;
 use crate::DayExecutable;
 
 pub struct Day4Executable {}
@@ -13,11 +14,45 @@ impl DayExecutable for Day4Executable {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+enum Entry {
+    Value(u32),
+    Matched,
+}
+
+#[derive(Debug)]
+struct Board {
+    id: u32,
+    entries: Vec<Vec<Entry>>,
+}
+
 impl Day4Executable {
     fn process(&self, lines: Vec<String>) {
         let numbers: Vec<u32> = lines.get(0).unwrap().split(',').map(|s| s.parse::<u32>().unwrap()).collect();
-        println!("{:?}", numbers);
-        let boardLines = &lines[2..];
-        println!("{:?}", boardLines);
+        let board_lines = &lines[2..];
+        let mut boards = init_boards(board_lines);
+        println!("{:?}", boards);
     }
+}
+
+fn init_boards(board_lines: &[String]) -> Vec<Board> {
+    let mut boards: Vec<Board> = Vec::new();
+    let mut entries: Vec<Vec<Entry>> = Vec::new();
+    let mut id = 1;
+    for board_line in board_lines {
+        if !board_line.is_empty() {
+            let row_entries = board_line.split(" ").filter(|v|!v.is_empty()).map(|v| Value(v.parse::<u32>().unwrap())).collect();
+            entries.push(row_entries);
+        }
+        if entries.len() == 5 {
+            let board = Board {
+                id,
+                entries: entries.clone()
+            };
+            boards.push(board);
+            id += 1;
+            entries = Vec::new();
+        }
+    }
+    return boards;
 }
