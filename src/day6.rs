e71fd487a98983f3f71538986254806ef4641fbc
Thomas Zub <thomas.zub@codecentric.de>
2021-12-17 10:29:53 +0100

    Solved day 6 part 1

--- src/day6.rs
+++ src/day6.rs
@@ -15,6 +15,26 @@ impl DayExecutable for Day6Executable {
 
 impl Day6Executable {
     fn process(&self, lines: Vec<String>) {
-
+        let mut state = get_initial_state(lines.get(0).unwrap());
+        for _ in 0..80 {
+            let mut new_fish = 0;
+            for s in state.iter_mut() {
+                match s {
+                    0 => {
+                        *s = 6;
+                        new_fish += 1;
+                    },
+                    _ => *s -= 1
+                }
+            }
+            for _ in 0..new_fish {
+                state.push(8);
+            }
+        }
+        println!("Count: {}", state.len());
     }
+}
+
+fn get_initial_state(line: &String) -> Vec<u32> {
+    line.split(",").map(|s| s.parse().unwrap()).collect()
 }
